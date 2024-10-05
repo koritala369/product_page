9adbfcf776ad9604a49d987055bc403f895326cf
koritala369 <koritalagopi8@gmail.com>
2024-10-05 22:34:33 +0530

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { WebpageComponent } from './webpage/webpage.component'; 
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(WebpageComponent, appConfig)
   .catch((err) => console.error(err));
